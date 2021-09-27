365a98bf908ebd157c6c5bcab987ac7841a57e16
whatwouldstevedo <valieffbulat@gmail.com>
2021-09-27 10:58:21 +0300

    release v1

---
+++ db/migrate/20210925171743_create_libraries.rb
@@ -0,0 +1,11 @@
+class CreateLibraries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :libraries do |t|
+      t.string  :name
+      t.string  :country
+      t.text    :body
+
+      t.timestamps
+    end
+  end
+end
