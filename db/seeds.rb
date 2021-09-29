3ee3ca87e28795859f9ff3527fcdc538d52ced80
whatwouldstevedo <valieffbulat@gmail.com>
2021-09-29 12:44:13 +0300

    fix and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-libraries = Library.create([{ name: 'Russian Government Library' }, { name: 'United States Government Library'},{ name: 'British Government Library' }])
-Author.create(name: 'Alexander', surname: 'Pushkin', country: 'Russia', libraries: library.first)
-Author.create(name: 'Kurt', surname: 'Vonnegut', country: 'USA', libraries: library.second)
-Author.create(name: 'Arthur Conan', surname: 'Doyle', country: 'UK', libraries: library.third)
+libraries = Library.create([{ name: 'Russian Government Library' }, { name: 'United States Government Library'}, { name: 'British Government Library' }])
+Author.create(name: 'Alexander', surname: 'Pushkin', country: 'Russia', library: libraries.first)
+Author.create(name: 'Kurt', surname: 'Vonnegut', country: 'USA', library: libraries.second)
+Author.create(name: 'Arthur Conan', surname: 'Doyle', country: 'UK', library: libraries.third)
